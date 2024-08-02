d23b6db96b1f5846e50b9cc0de83b265aca2dc9b
Johannes Thorén <johannes@lgjt.xyz>
2024-08-02 20:29:11 +0200

    added style

--- src/api.rs
+++ src/api.rs
@@ -1,15 +1,21 @@
-use rocket::serde::json::Json;
+use rocket::{form::Form, response::content, serde::json::Json};
 use sqlx::SqlitePool;
 
 use crate::{database::*, models::url::Url};
 
-#[get("/new_url?<url>")]
-pub async fn new_url(url: &str, pool: &rocket::State<SqlitePool>) -> Result<String, String> {
-    match insert_new_url(url.to_string(), pool.inner()).await {
+#[post("/url", data = "<url_input>")]
+pub async fn new_url(
+    url_input: Form<String>,
+    pool: &rocket::State<SqlitePool>,
+) -> Result<String, String> {
+    match insert_new_url(url_input.to_string(), pool.inner()).await {
         Ok(id) => match get_url_from_id(id.clone(), pool).await {
             Ok(res) => {
                 info!("{:?}", res);
-                Ok(id)
+                Ok(format!(
+                    "<a href=\"/u/{0}\" id=\"shortend-url\">{0}</a>",
+                    res.id
+                ))
             }
             Err(e) => Err(e.to_string()),
         },
@@ -17,10 +23,21 @@ pub async fn new_url(url: &str, pool: &rocket::State<SqlitePool>) -> Result<Stri
     }
 }
 
-#[get("/get_info/<id>")]
-pub async fn get_info(id: &str, pool: &rocket::State<SqlitePool>) -> Json<Url> {
-    match get_url_from_id(id.to_string(), pool).await {
-        Ok(r) => return Json(r),
-        Err(_) => todo!(),
+#[get("/url?<urlid>")]
+pub async fn get_info(urlid: &str, pool: &rocket::State<SqlitePool>) -> String {
+    match get_url_from_id(urlid.to_string(), pool).await {
+        Ok(r) => {
+            return format!(
+                r"
+            <p>clicks {}</p>
+            <p>url {}</p>
+            <p>created {}</p>
+        ",
+                r.clicks,
+                r.url,
+                r.created.to_string()
+            )
+        }
+        Err(_) => return String::from("<p>Not Found!</p>"),
     }
 }
