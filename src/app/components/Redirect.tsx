b11fe11acaa7f7e10904bedfc9b8e2ccb8872c5e
Johannes Thorén <Johannes@lgjt.xyz>
2024-10-14 19:11:42 +0200

    some css fix

--- src/app/components/Redirect.tsx
+++ src/app/components/Redirect.tsx
@@ -3,20 +3,20 @@
 import { useState } from "react";
 
 export default function ({ url }: { url: String }) {
-    let [count, setCount] = useState(1);
-    let count_max = 5;
+  let [count, setCount] = useState(1);
+  let count_max = 5;
 
-    setInterval(() => {
-        setCount(count + 1);
-        if (count == count_max) {
-            window.location.replace(url.toString());
-        }
-    }, 1000);
+  setInterval(() => {
+    setCount(count + 1);
+    if (count == count_max) {
+      window.location.replace(url.toString());
+    }
+  }, 1000);
 
-    return (
-        <div>
-            <h1>Redirecting to {url}</h1>
-            <p>in {count_max + 1 - count} seconds</p>
-        </div>
-    );
+  return (
+    <div>
+      <h1 className="text-4xl">Redirecting in {count_max + 1 - count}</h1>
+      <p>source: <span className="text-xs">{url}</span></p>
+    </div>
+  );
 }
--- src/app/info/[id]/page.tsx
+++ src/app/info/[id]/page.tsx
@@ -1,39 +1,41 @@
 import client from "../../../../prisma/db";
 
 export default async function ({ params }: any) {
-    const { id } = params;
+  const { id } = params;
 
-    const url = await (async () => {
-        return client.url.findUnique({ where: { id: id } });
-    })();
+  const url = await (async () => {
+    return client.url.findUnique({ where: { id: id } });
+  })();
 
-    return (
-        <main className="grid place-items-center h-screen">
-            <div>
-                <h1 className="text-xl underline  ">
-                    <a href={`${process.env.SHORT_ROOT_URL}/u/${id}`}>
-                        {process.env.SHORT_ROOT_URL}/u/{id}
-                    </a>
-                </h1>
-                <table className="">
-                    <tbody>
-                        <tr>
-                            <th className="text-left">source</th>
-                            <td className="text-right">{url?.source}</td>
-                        </tr>
-                        <tr>
-                            <th className="text-left">created</th>
-                            <td className="text-right">
-                                {url?.created.toISOString()}
-                            </td>
-                        </tr>
-                        <tr>
-                            <th className="text-left">clicks</th>
-                            <td className="text-right">{url?.clicks}</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </main>
-    );
+  return (
+    <main className="grid place-items-center h-screen">
+      <div>
+        <h1 className="text-xl underline  ">
+          <a href={`${process.env.SHORT_ROOT_URL}/u/${id}`}>
+            {process.env.SHORT_ROOT_URL}/u/{id}
+          </a>
+        </h1>
+        <table className="">
+          <tbody>
+            <tr>
+              <th className="text-left">source</th>
+              <td className="text-right overflow-hidden text-xs">
+                {url?.source}
+              </td>
+            </tr>
+            <tr>
+              <th className="text-left">created</th>
+              <td className="text-right text-xs">
+                {url?.created.toISOString()}
+              </td>
+            </tr>
+            <tr>
+              <th className="text-left">clicks</th>
+              <td className="text-right text-xs">{url?.clicks}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </main>
+  );
 }
